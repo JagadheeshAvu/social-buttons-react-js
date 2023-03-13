b1d7f980e2a63e3e0666c5a2cc11a7ebbd52e4b6
JagadheeshAvu <jagadheesh6349@gmail.com>
2023-03-13 06:11:34 +0000

    Social Buttons Solution React JS

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const Button = (props) => {
   //  Write your code here.
-  const {className, buttonText} = props;
-  return (<button className='${className}'>{buttonText}</button>;)
+  const { className, buttonText } = props;
+  return <button className={`button ${className}`}>{buttonText}</button>;
 };
 
 const element = (
   //  Write your code here.
-  <div className="bg-container d-flex flex-column justify-content-center">
+  <div className="bg-container">
     <h1 className="heading">Social Buttons</h1>
-    <div className="text-center">
-    <Button className="like-btn" buttonText="Like"/>
-    <Button className="comment-btn" buttonText="Comment"/>
-    <Button className="share-btn" buttonText="Share"/>
+    <div className="btn-container">
+      <Button className="like-btn" buttonText="Like" />
+      <Button className="comment-btn" buttonText="Comment" />
+      <Button className="share-btn" buttonText="Share" />
     </div>
   </div>
 );
